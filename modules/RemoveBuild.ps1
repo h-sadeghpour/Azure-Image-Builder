1fd3b71fcf6fb43940f18b33109bcedf9e4e99d2
Hamid Sadeghpour Saleh <49187548+h-sadeghpour@users.noreply.github.com>
2023-05-15 11:49:24 +0200

    Update RemoveBuild.ps1

--- modules/RemoveBuild.ps1
+++ modules/RemoveBuild.ps1
@@ -1,1 +1,19 @@
+#Variables
+    $resourceGroupName = "rg-avdaib-prd-we-03"
+    $imageTemplateName = "avd10ImageTemplate03"
+    
+    Install-Module Az.ImageBuilder -force
+    Import-Module Az.ImageBuilder
+    
+#Verify the previous Template
 
+    $Template = Get-AzImageBuilderTemplate
+    If ($Template.Name -like "avd10ImageTemplate03"){
+    
+        Remove-AzImageBuilderTemplate -Name $Template.Name -ResourceGroupName $Template.ResourceGroupName
+    }Else {
+    
+        Write-Host "Template does not exist and will be created." 
+    }
+    
+    Start-Sleep -Seconds 300
