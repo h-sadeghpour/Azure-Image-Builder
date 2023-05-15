6a048a5790433a81af20d1f60d0c75c99e3ecdd3
Hamid Sadeghpour Saleh <49187548+h-sadeghpour@users.noreply.github.com>
2023-05-15 11:28:47 +0200

    Update startBuild.ps1

--- modules/startBuild.ps1
+++ modules/startBuild.ps1
@@ -7,14 +7,16 @@
     
 #Verify the previous Template
 
-    #$Template = Get-AzImageBuilderTemplate
-    #If ($Template.Name -like "avd10ImageTemplate03"){
+    $Template = Get-AzImageBuilderTemplate
+    If ($Template.Name -like "avd10ImageTemplate03"){
     
-    #    Remove-AzImageBuilderTemplate -Name $Template.Name -ResourceGroupName $Template.ResourceGroupName
-    #}Else {
+        Remove-AzImageBuilderTemplate -Name $Template.Name -ResourceGroupName $Template.ResourceGroupName
+    }Else {
     
-    #    Write-Host "Template does not exist and will be created." 
-    #}
+        Write-Host "Template does not exist and will be created." 
+    }
+    
+    Start-Sleep -Seconds 300
 
 #Build image
     Start-AzImageBuilderTemplate -ResourceGroupName $resourceGroupName -Name $imageTemplateName -NoWait
