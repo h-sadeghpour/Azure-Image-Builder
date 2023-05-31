7678a592a753496ca0eed40f4cd08c9a5499f36a
Hamid Sadeghpour Saleh <49187548+h-sadeghpour@users.noreply.github.com>
2023-05-31 13:31:07 +0200

    Update startBuild.ps1

--- modules/startBuild.ps1
+++ modules/startBuild.ps1
@@ -1,29 +1,34 @@
-#Variables
+### Variables
     $resourceGroupName = "rg-avdaib-prd-we-03"
     $imageTemplateName = "avd10ImageTemplate03"
+    $galleryName = "acg_avdcomputegallary_prd_we_03"
+    $imageDefinitionName = "wind10avd22h2"
+    $imageversion = Get-AzGalleryImageVersion -ResourceGroupName rg-avdaib-prd-we-03 -GalleryName acg_avdcomputegallary_prd_we_03 -GalleryImageDefinitionName wind10avd22h2 | Sort-Object -Bottom 1;
+    $region1 = @{Name='West Europe';ReplicaCount=1}
+    $region2 = @{Name='southeastasia';ReplicaCount=1}
+    $regions = @($region1,$region2)
+    
     
     Install-Module Az.ImageBuilder -force
     Import-Module Az.ImageBuilder
 
-#Build image
+### Build image
     Start-AzImageBuilderTemplate -ResourceGroupName $resourceGroupName -Name $imageTemplateName -NoWait
 
-#Get build status
+### Get build status
     $buildStatus=$(Get-AzImageBuilderTemplate -ResourceGroupName $resourceGroupName -Name $imageTemplateName)
     $buildStatus | Format-List -Property *
     $buildStatus.LastRunStatusRunState 
     $buildStatus.LastRunStatusRunSubState
     
-    #Start-Sleep -Seconds 7200
+    ### Wait Here until the new Image Version Created
     
-    #Verify the previous Template
-
-    #$Template = Get-AzImageBuilderTemplate
-    #If ($Template.Name -like "avd10ImageTemplate03"){
+    Start-Sleep -Seconds 10000
     
-    #    Remove-AzImageBuilderTemplate -Name $Template.Name -ResourceGroupName $Template.ResourceGroupName
-    #}Else {
+    ### Add New Regional Replication Option to the Image
     
-    #    Write-Host "Template does not exist and will be created." 
-    #}
+    Update-AzGalleryImageVersion -ResourceGroupName $resourceGroupName -GalleryName $galleryName -GalleryImageDefinitionName $imageDefinitionName -Name $imageversion.Name -ReplicaCount 1 -TargetRegion $regions
+    
+    Write-Host "Image Version Created and Replicated to all necessary regions!"
+    
     
